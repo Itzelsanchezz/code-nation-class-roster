fa488eb4545698e09799e10d31ee3e06dfae5eef
Itzelsanchezz <72948547+Itzelsanchezz@users.noreply.github.com>
2021-11-08 17:39:32 -0600

    Code Nation - items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+function createrosterItem(firstName, lastName, pronouns, studentHS) {
+  let rosterItem = (
+    <div class="row">
+      <div class="name">
+        {firstName} {lastName}{" "}
+      </div>
+      <div class="pronouns">{pronouns}</div>
     </div>
   );
+  return rosterItem;
+}
+
+export default function App() {
+  let rosterItem = createrosterItem(
+    "Mikkail Allen",
+    "he/his",
+    "Williamsburg",
+    "blue",
+    "2022"
+  );
+
+  return rosterItem;
 }
